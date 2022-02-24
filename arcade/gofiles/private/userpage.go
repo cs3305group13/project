949c7b3381f7d1a8ce1ccdc1c2d994f23e81d268
JakubRuminski <jakubruminski09@gmail.com>
2022-02-24 17:24:52 +0000

    also implemented userpage

--- arcade/gofiles/private/userpage.go
+++ arcade/gofiles/private/userpage.go
@@ -1,6 +1,40 @@
 package private
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/cs3305/group13_2022/project/utils/token"
+)
 
 func HandleUserPage(w http.ResponseWriter, r *http.Request) {
+	if token.TokenValid(w, r, true) {
+		checkForm(w, r)
+	}
+}
+
+
+type UserPage struct {
+	Username string
+}
+
+func checkForm( w http.ResponseWriter, r *http.Request ) {
+
+	r.ParseForm()
+	if len(r.Form) == 0 {
+		username := token.GetUsername( r, "token" )
+		attachPage( w, "userpage.html", UserPage{ Username: username })
+		return
+	} else {
+		// gameSetup := poker.HandlePokerForm(w, r)
+		// if gameSetup {
+		//     http.Redirect(w, r, "pokertable", http.StatusMovedPermanently)
+		// }
+	}
+}
+
+func attachPage( w http.ResponseWriter, htmlName string, userPage UserPage) {
+	var tpl *template.Template
+	tpl, _ = template.ParseGlob("templates/private/" + htmlName)
+	tpl.ExecuteTemplate(w, htmlName, userPage)
 }
