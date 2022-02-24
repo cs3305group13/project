e8f4c1cea55ff8d0d79ec207e8e3a8a5a0ed23ae
Eoin <119492694@umail.ucc.ie>
2022-02-24 17:22:26 +0000

    Fully connected pages to handler.go

--- arcade/handler.go
+++ arcade/handler.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/cs3305/group13_2022/project/arcade/gofiles/private"
 	"github.com/cs3305/group13_2022/project/arcade/gofiles/public"
 )
 
@@ -10,8 +12,15 @@ func main() {
 	http.HandleFunc("/", public.HandleLoginPage)
 	http.HandleFunc("/signup", public.HandleSignupPage)
 
-	http.HandleFunc("/userpage", public.HandleUserPage)
+	http.HandleFunc("/userpage", private.HandleUserPage)
 
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	fmt.Print("\033[H\033[2J") // clears terminal
+	fmt.Println("")
+	fmt.Println("##################################################")
+	fmt.Println("Server running on localhost: http://localhost:9000")
+	fmt.Println("##################################################")
 
 	http.ListenAndServe("localhost:9000", nil)
 }
