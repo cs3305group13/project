156b426cd70fed459d430adb50eff54288ff6d1b
Eoin <119492694@umail.ucc.ie>
2022-02-24 17:50:11 +0000

    Improved getnextavailableplayers method, altered tests accordingly

--- poker/mysql_poker/gameflow/gameflow.go
+++ poker/mysql_poker/gameflow/gameflow.go
@@ -31,7 +31,7 @@ func UpdateUsersTimeSinceRequest(DB *mysql_db.DB, tablesTableName, playersTableN
 }
 
 // Method used to update next player who holds the responsibility.
-//
+// 
 // setOperation := "highest_bidder = "
 func SetNextAvailablePlayerAfterThisOne(DB *mysql_db.DB, tx *sql.Tx, tableName, playersTableName, tableID, username, seatNumber, setOperation string) {
 	playerName := NextAvailablePlayer(DB, playersTableName, tableID, username, seatNumber)
@@ -40,6 +40,7 @@ func SetNextAvailablePlayerAfterThisOne(DB *mysql_db.DB, tx *sql.Tx, tableName, 
 	query := fmt.Sprintf(`UPDATE %s
 	                      SET %s
 						  WHERE table_id = %s;`, tableName, setOperation, tableID)
+
 
 	res, err := tx.Exec(query)
 	utils.CheckError(err)
@@ -50,12 +51,12 @@ func SetNextAvailablePlayerAfterThisOne(DB *mysql_db.DB, tx *sql.Tx, tableName, 
 }
 
 // return next available players who are not idle nor in 'NOT_READY', 'LEFT', 'FOLDED', and 'ALL_IN' state.
-func NextAvailablePlayers(DB *mysql_db.DB, playersTableName, tableID, username, seatNumber string) (playersAfter, playersBefore *sql.Rows) {
+func NextAvailablePlayers(DB *mysql_db.DB, playersTableName, tableID, username, seatNumber string) (playerNames []string) {
 
 	// TODO: Make "5" seconds into an env variable.
 	db := mysql_db.EstablishConnection(DB)
 	defer db.Close()
-
+	
 	// time_since_request > DATE_SUB(NOW(), INTERVAL %s SECOND) is used here to prevent selecting idle players
 	query := fmt.Sprintf(`SELECT username
 						  FROM %s
@@ -70,42 +71,47 @@ func NextAvailablePlayers(DB *mysql_db.DB, playersTableName, tableID, username, 
 	playersAfter, err := db.Query(query)
 	utils.CheckError(err)
 
-	query = fmt.Sprintf(` SELECT username
-						  FROM %s
-						  WHERE table_id = %s AND
-						        seat_number <= %s AND
-								player_state != "NOT_READY" AND
-								player_state != "LEFT" AND
-								player_state != "FOLDED" AND
-								player_state != "ALL_IN" AND
-								time_since_request > DATE_SUB(NOW(), INTERVAL %s SECOND)
-						  ORDER BY seat_number ASC;`, playersTableName, tableID, seatNumber, "5") // seat_number used to be `<` it is `<=` because of dealer, big, small blinds overlap.
+	var playerName string
 
-	playersBefore, err = db.Query(query)
+	for playersAfter.Next() {
+		err := playersAfter.Scan(&playerName)
+		utils.CheckError(err)
+		playerNames = append(playerNames, playerName)
+	}
+	playersAfter.Close()
+
+	query = fmt.Sprintf(`SELECT username
+						 FROM %s
+						 WHERE table_id = %s AND
+						       seat_number <= %s AND
+						       player_state != "NOT_READY" AND
+							   player_state != "LEFT" AND
+							   player_state != "FOLDED" AND
+							   player_state != "ALL_IN" AND
+							   time_since_request > DATE_SUB(NOW(), INTERVAL %s SECOND)
+						 ORDER BY seat_number ASC;`, playersTableName, tableID, seatNumber, "5")  // seat_number used to be `<` it is `<=` because of dealer, big, small blinds overlap.
+	
+	playersBefore, err := db.Query(query)
 	utils.CheckError(err)
 
-	return playersAfter, playersBefore
+	for playersBefore.Next() {
+		err := playersBefore.Scan(&playerName)
+		utils.CheckError(err)
+		playerNames = append(playerNames, playerName)
+	}
+	playersBefore.Close()
+
+	return playerNames
 }
 
 // return next available player who is not idle nor in 'NOT_READY', 'LEFT', 'FOLDED', and 'ALL_IN' state.
 func NextAvailablePlayer(DB *mysql_db.DB, playersTableName, tableID, username, seatNumber string) (playerName string) {
+	
+	playerNames := NextAvailablePlayers(DB, playersTableName, tableID, username, seatNumber)
 
-	playersAfter, playersBefore := NextAvailablePlayers(DB, playersTableName, tableID, username, seatNumber)
-
-	// tries to find a player next in line ie if user is at index 4 it returns anyone who is at 5, 6, 7
-	for playersAfter.Next() {
-		err := playersAfter.Scan(&playerName)
-		utils.CheckError(err)
-		return playerName
+	if len(playerNames) != 0 {
+		playerName = playerNames[0]
 	}
 
-	// tries to find a player before ie if user is at index 4 it returns anyone who is at 0, 1, 2, 3
-	for playersBefore.Next() {
-		err := playersBefore.Scan(&playerName)
-		utils.CheckError(err)
-		return playerName
-	}
-
-	// if here, no one was found
-	return
+	return playerName
 }
---
+++ poker/mysql_poker/gameflow/gameflow_test.go
@@ -0,0 +1,55 @@
+package gameflow
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/cs3305/group13_2022/project/mysql_db"
+	"github.com/cs3305/group13_2022/project/testing/mysql_poker"
+	"github.com/cs3305/group13_2022/project/utils/env"
+)
+
+
+var envs = env.GetEnvironmentVariables("../../../testing.env")
+	
+var DB = mysql_db.NewDB(envs)
+
+var testingTablesTableName = envs["TESTING_TABLES_TABLE"]
+var testingPlayersTableName = envs["TESTING_PLAYERS_TABLE"]
+var testingPokerTableName = envs["TESTING_POKER_TABLES_TABLE"]
+
+func TestNextAvailablePlayer(t *testing.T) {
+
+	mysql_poker.RefreshPlayers(DB, testingPlayersTableName)
+
+	tableID := "1"
+	
+	NextAvailablePlayer(DB, testingPlayersTableName, tableID, "derek", "1")
+}
+
+
+func TestNextAvailablePlayers(t *testing.T) {
+
+	mysql_poker.RefreshPlayers(DB, testingPlayersTableName)
+
+	tableID := "1"
+	playerNames := NextAvailablePlayers(DB, testingPlayersTableName, tableID, "derek", "1")
+
+	// No output because 5 seconds in where clause
+	for i:=0; i<len(playerNames); i++ {
+		fmt.Println(playerNames[i])
+	}
+}
+
+func TestSetNextAvailablePlayerAfterThisOne(t *testing.T) {
+	mysql_poker.RefreshPlayers(DB, testingPlayersTableName)
+
+	db := mysql_db.EstablishConnection(DB)
+	tx := mysql_db.NewTransaction(db)
+	defer tx.Rollback()
+	defer db.Close()
+
+	tableID := "1"
+	
+	SetNextAvailablePlayerAfterThisOne(DB, tx, testingPokerTableName, testingPlayersTableName, tableID, "derek", "1", "dealer = ")
+}
