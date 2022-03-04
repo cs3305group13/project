d213e6cbdd484655177f9976cfec1f0a7be5d5e1
jakub <jakubruminski09@gmail.com>
2022-03-04 09:07:52 +0000

    Cleaned up methods in user_request.go

--- poker/user_request.go
+++ poker/user_request.go
@@ -46,9 +46,8 @@ func handleUserButtons( w http.ResponseWriter, r *http.Request, envs map[string]
 		return
 	} 
 	tableID := token.GetTableID(r, "token")
-	inProgress := gameinteraction.GameInProgress(w,DB, tablesTableName, tableID)
+	inProgress := gameinfo.GameInProgress( DB, tablesTableName, tableID )
 	if inProgress {
-
 		username := token.GetUsername(r, "token")
 
 		// checks if players turn.
@@ -58,32 +57,70 @@ func handleUserButtons( w http.ResponseWriter, r *http.Request, envs map[string]
 			return
 		} else {
 
-			db := mysql_db.EstablishConnection(DB)
-			tx := mysql_db.NewTransaction(db)
-			defer tx.Rollback()
-			defer db.Close()
-
 			seatNumber := token.GetSeatNumber(r, "token")
 
 			action := html.EscapeString(r.Form.Get("action"))
 			if action == "Fold" {
-				gameinteraction.PlayerFolded(DB, tx, tablesTableName, playersTableName, pokerTablesTableName, tableID, username, seatNumber)
+				FoldPlayer(DB, tablesTableName, playersTableName, pokerTablesTableName, tableID, username, seatNumber)	
 			} 
 
 			amount := html.EscapeString(r.Form.Get("amount"))
 			if ( action == "Raise" || action == "Call" || action == "Check" ) && amount != "" {
-				gameinteraction.PlayerTakesAction( DB, tx, tablesTableName, playersTableName, pokerTablesTableName, tableID, username, seatNumber, amount )
+				RaiseCallCheckPlayer(DB, tablesTableName, playersTableName, pokerTablesTableName, tableID, username, seatNumber, amount)
 
 				highestBidder, _ := gameinfo.GetHighestBidder(DB, pokerTablesTableName, tableID)
 				nextAvailablePlayer := gameflow.NextAvailablePlayer(DB, playersTableName, tableID, username, seatNumber)
 				if nextAvailablePlayer == highestBidder {
-					gamecards.AddToCommunityCards(DB, tx, tablesTableName, pokerTablesTableName, tableID)
+					EndRound(DB, tablesTableName, playersTableName, pokerTablesTableName, tableID)
 				}
 			}
-
-
-			err := tx.Commit()
-			utils.CheckError(err)
 		}
 	}
+}
+
+// Creates a transaction which handles folding player.
+func FoldPlayer(DB *mysql_db.DB, tablesTableName, playersTableName, pokerTablesTableName, tableID, username, seatNumber string) {
+
+	db := mysql_db.EstablishConnection(DB)
+	tx := mysql_db.NewTransaction(db)
+	defer tx.Rollback()
+	defer db.Close()
+
+	setOperation := "current_player_making_move = "
+	successful := gameflow.SetNextAvailablePlayerAfterThisOne(DB, tx, tablesTableName, playersTableName, tableID, username, seatNumber, setOperation)
+	
+	gameinteraction.PlayerFolded(DB, tx, tablesTableName, playersTableName, pokerTablesTableName, tableID, username, seatNumber, successful)
+
+	err := tx.Commit()
+	utils.CheckError(err)
+}
+
+// Creates a transaction which handles ( raising || calling || checking ) player.
+func RaiseCallCheckPlayer(DB *mysql_db.DB, tablesTableName, playersTableName, pokerTablesTableName, tableID, username, seatNumber, amount string) {
+
+	db := mysql_db.EstablishConnection(DB)
+	tx := mysql_db.NewTransaction(db)
+	defer tx.Rollback()
+	defer db.Close()
+
+	gameinteraction.PlayerTakesAction( DB, tx, tablesTableName, playersTableName, pokerTablesTableName, tableID, username, seatNumber, amount )
+
+	err := tx.Commit()
+	utils.CheckError(err)
+}
+
+// Creates a transaction which handles adding to community cards.
+func EndRound(DB *mysql_db.DB, tablesTableName, playersTableName, pokerTablesTableName, tableID string) {
+
+	db := mysql_db.EstablishConnection(DB)
+	tx := mysql_db.NewTransaction(db)
+	defer tx.Rollback()
+	defer db.Close()
+
+	gamecards.AddToCommunityCards(DB, tx, tablesTableName, pokerTablesTableName, tableID)
+	// TODO: 
+	gameflow.ClearUsersMoneyInPot(DB, tx, playersTableName, pokerTablesTableName, tableID)
+
+	err := tx.Commit()
+	utils.CheckError(err)
 }
