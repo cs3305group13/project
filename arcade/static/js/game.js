fdb93fbef2c90e707aaefcffc9c2de6d53152597
eoinomahony146 <eoinomahony146@gmail.com>
2022-03-08 18:58:29 +0000

    fixed double prints, implemented hit(), stand() and score()

--- arcade/static/js/game.js
+++ arcade/static/js/game.js
@@ -5,9 +5,9 @@ let suits = ["s", "h", "d", "c"];
 let facevalues = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k", "a"];
 let deck = new Array();
 let dealerCards = new Array();
-let dealerScore = new Array();
+let dealerScore = 0;
 let playerCards = new Array();
-let playerScore = new Array();
+let playerScore = 0;
 
 function init() {
 
@@ -38,7 +38,7 @@ function createDeck() {
             if (facevalues[i] == "j" || facevalues[i] == "q" || facevalues[i] == "k")
                 weight = 10;
             if (facevalues[i] == "a")
-                weight = 11 || 1;
+                weight = 11;
             let card = { Value: facevalues[i], Suit: suits[x], Weight: weight };
             deck.push(card);
         }
@@ -53,7 +53,6 @@ function shuffle() {
         deck[i] = deck[rndNo];
         deck[rndNo] = card;
     }
-    console.log(deck)
 }
 
 playersList = new Array();
@@ -62,14 +61,16 @@ playersList = new Array();
 function players() {
     let player1 =  document.getElementById('user1').value;
     if( player1 != "") {
-        playersList.push(player1)
-    console.log("User 1: " + player1)
+        playersList.push(player1);
+    console.log("User 1: " + player1);
     }
 }
 
 function deal() {
     playerCards = [nextCard(), nextCard()]
-    dealerCards = [nextCard(), nextCard()]
+    dealerCards = [nextCard()]
+    Score();
+    // console.log(playerCards)
 }
 
 function nextCard() {
@@ -77,9 +78,37 @@ function nextCard() {
 }
 
 function Hit() {
-
+    playerCards.push(deck.shift());
+    Score();
 }
 
 function Stand() {
+    if (dealerScore < 17)
+        dealerCards.push(deck.shift());
+        Score();
+} 
 
+function Score() {
+    for(i = 0; i < playerCards.length; i ++) {
+        playerScore += playerCards[i].Weight;
+        console.log(playerCards);
+        console.log(playerScore);
+    }
+
+    for(i = 0; i < dealerCards.length; i ++) {
+        dealerScore += dealerCards[i].Weight;
+        console.log(dealerCards);
+        console.log(dealerScore);
+    }
+
+
+    if (playerScore > 21) {
+        for (i = 0; i < playerCards.length; i ++) {
+            if (playerCards[i].Value == "a" && playerCards[i].Weight == 11) {
+                playerCards[i].Weight == 1;
+            } else {
+            console.log("Dealer wins!")
+            }
+        }
+    }
 }
