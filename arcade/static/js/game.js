9bfd8af236d312b136dd504933207034d28855b7
eoinomahony146 <eoinomahony146@gmail.com>
2022-03-11 18:16:41 +0000

    Added comments, removed redundant variables

--- arcade/static/js/game.js
+++ arcade/static/js/game.js
@@ -16,7 +16,9 @@ function init() {
     btnHit.addEventListener( "click", Hit, false );
     btnStand.addEventListener( "click", Stand, false );
 }
-function startGame() {  
+
+function startGame() {
+    // function to initialize the game  
     createDeck();
     shuffle(); 
     deal();
@@ -27,6 +29,7 @@ function getRandomInt(min, max) {
 }
 
 function createDeck() {
+    // function that creates and assignes weights to cards in the deck
     deck = new Array(); // reset deck
 
     for (let i = 0; i < facevalues.length; i++) {
@@ -43,6 +46,8 @@ function createDeck() {
 }
 
 function shuffle() {
+    // function that shuffles the created deck by swapping a card at a given 
+    // index with a random card in the deck
     for(let i = 0; i < 52; i++) {
         let rndNo = getRandomInt(1,52);
         let card = deck[i];
@@ -52,31 +57,34 @@ function shuffle() {
     }
 }
 
-playersList = new Array();
-
 function deal() {
+    // function that initially deals the player 2 cards, and the dealer one.
     playerCards = [nextCard(), nextCard()]
     dealerCards = [nextCard()]
     Score();
 }
 
 function nextCard() {
+    // function that returns the next available card from the deck and removes it from the deck
     return deck.shift();
 }
 
 function Hit() {
+    // function that deals the player another card and recalculates their score
     playerScore = 0;
     playerCards.push(deck.shift());
     Score();
 }
 
 function Stand() {
+    // function that deals the dealer another card
     if (dealerScore < 21)
         dealerCards.push(deck.shift());
         Score();
 } 
 
 function Score() {
+    // logic of blackjack implemented 
     playerScore = 0;
     for(i = 0; i < playerCards.length; i ++) {
         playerScore += playerCards[i].Weight;
@@ -92,6 +100,7 @@ function Score() {
     }
     
     if (playerScore > 21) {
+        // change the value of "A" from 11 if the score is over 21 to 1.
         for (i = 0; i < playerCards.length; i ++) {
             if (playerCards[i].Value == "a" && playerCards[i].Weight == 11) {
                 playerCards[i].Weight == 1;
@@ -108,5 +117,5 @@ function Score() {
         console.log("Dealer wins")
     } if (playerScore == 21 && dealerScore != 21) {
         console.log("Player wins")
-}
+    }
 }
